9383855e5a651deeabd5f7062f0fadbc98aea30e
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2021-06-26 13:23:30 +0200

    Projekt1 Next

--- Projekt1/main.js
+++ Projekt1/main.js
@@ -5,10 +5,47 @@ function recalculate() {
         el => el.value ? parseInt(el.value) : null
     ).filter(el => typeof el === 'number');
     var results = document.getElementById('results'), resultText = '';
+    if (elements.length < 1) {
+        results.innerHTML = '';
+        return;
+    }
     var sum = elements.reduce((value, el) => value + el, 0);
     resultText = 'Suma: ' + sum;
     resultText += '<br/>Średnia: ' + sum/elements.length;
     resultText += '<br/>Min: ' + Math.min(...elements);
     resultText += '<br/>Max: ' + Math.max(...elements);
     results.innerHTML = resultText;
+}
+
+function updateInputs() {
+    var count = parseInt(document.getElementById('input-count').value);
+    if (typeof count === 'NaN') return;
+    var content = document.getElementById('inputs');
+    var oldCount = content.childNodes.length-1;
+    for (var i = oldCount; i !== count; i < count ? i++ : i--) {
+        if (i < count) {
+            let input = new Input();
+            input.appendTo(content);
+        } else {
+            content.removeChild(content.lastChild);
+        }
+    }
+    recalculate();
+}
+
+class Input {
+    node;
+
+    constructor() {
+        this.node = document.createElement('div');
+        let input = document.createElement('input');
+        input.className = 'input-number';
+        input.type = 'number';
+        input.onchange = recalculate
+        this.node.appendChild(input);
+    }
+
+    appendTo(parent) {
+        parent.appendChild(this.node);
+    }
 }
